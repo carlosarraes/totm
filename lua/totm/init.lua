e5052a065bd5fb4c068f686912625bb8c90a7aee
carraes <carraeshb@gmail.com>
2023-11-26 02:24:42 -0300

    feat: send data with curl to ollama

--- lua/totm/init.lua
+++ lua/totm/init.lua
@@ -1,4 +1,3 @@
-print("hello")
 local function copy_selection_to_new_buffer()
 	local bufnr = vim.api.nvim_get_current_buf()
 	local start_pos = vim.api.nvim_buf_get_mark(bufnr, "<")
@@ -23,6 +22,52 @@ local function copy_selection_to_new_buffer()
 	vim.cmd("vsplit")
 	vim.api.nvim_set_current_buf(new_buffer)
 end
+
+local function send_buffer_to_api()
+	print("sending buffer to api")
+	local bufnr = vim.api.nvim_get_current_buf()
+	local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+	local payload = table.concat(lines, "\\n")
+
+	-- Sanitization of the payload, replacing control characters
+	payload = payload:gsub("\t", "\\t"):gsub("\n", "\\n"):gsub('"', '\\"')
+
+	local model = "codellama:7b"
+	local prompt = string.format('{"model": "%s", "prompt": "%s", "stream": false}', model, payload)
+
+	-- Debug: Print the JSON payload to check its structure
+	print("JSON Payload: ", prompt)
+
+	local function on_event(_, data, event)
+		if event == "stdout" or event == "stderr" then
+			for _, line in ipairs(data) do
+				if line ~= "" then
+					vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, { line })
+				end
+			end
+		end
+	end
+
+	local cmd = {
+		"curl",
+		"-X",
+		"POST",
+		"http://localhost:11434/api/generate",
+		"-H",
+		"Content-Type: application/json",
+		"-d",
+		prompt,
+	}
+
+	vim.fn.jobstart(cmd, {
+		on_stderr = on_event,
+		on_stdout = on_event,
+		stdout_buffered = true,
+		stderr_buffered = true,
+	})
+	print("sent buffer to api")
+end
+
 vim.api.nvim_set_keymap(
 	"v",
 	"<leader>c",
@@ -30,6 +75,14 @@ vim.api.nvim_set_keymap(
 	{ noremap = true, silent = true }
 )
 
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>a",
+	':lua require("totm").send_buffer_to_api()<cr>',
+	{ noremap = true, silent = true }
+)
+
 return {
 	copy_selection_to_new_buffer = copy_selection_to_new_buffer,
+	send_buffer_to_api = send_buffer_to_api,
 }
